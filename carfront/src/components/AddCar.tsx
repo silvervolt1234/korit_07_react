832810734a110a830a17bbe06659d4bf1eb98abc
silvervolt1234 <105256888+silvervolt1234@users.noreply.github.com>
2025-10-22 14:40:31 +0900

    feat: before test

---
+++ carfront/src/components/AddCar.tsx
@@ -0,0 +1,65 @@
+import { Dialog, DialogActions, DialogTitle, Button } from "@mui/material";
+import { ChangeEvent, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addCar } from "../api/carapi";
+import CarDialogContent from "./CarDialogContent";
+
+function AddCar() {
+  const [ open, setOpen ] = useState(false);
+  const [ car, setCar ] = useState({
+    brand: '',
+    model: '',
+    color: '',
+    registrationNumber: '',
+    modelYear: 0,
+    price: 0
+  });
+
+  const handleClickOpen = () => setOpen(true);
+
+  const handleClickClose = () => setOpen(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCar({...car, [event.target.name]: event.target.value});
+  }
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(addCar, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cars"]);
+    },
+    onError: err => {
+      console.log(err);
+    },
+  });
+
+  const handleSave = () => {
+    mutate(car);
+    setCar({
+      brand: '',
+      model: '',
+      color: '',
+      registrationNumber: '',
+      modelYear: 0,
+      price: 0
+    });
+    handleClickClose();
+  }
+
+  return(
+    <>
+      <Button onClick={handleClickOpen} variant="outlined">New Car</Button>
+      <Dialog open={open} >
+        <DialogTitle>New Car</DialogTitle>
+        <CarDialogContent car={car} handleChange={handleChange}/>
+        <DialogActions>
+          <Button onClick={handleSave}>Save | 저장</Button>
+          <Button onClick={handleClickClose}>Cancel | 취소</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+export default AddCar
